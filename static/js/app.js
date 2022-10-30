98ea9dea32a18c8b701b3d54974d11480bc71e34
Dillon Dolejsi <dillon41393@sbcglobal.net>
2022-10-29 19:17:47 -0500

    updated bar chart code

--- static/js/app.js
+++ static/js/app.js
@@ -33,15 +33,42 @@ function buildChart(sample) {
         var labels = result.otu_labels;
         var values = result.sample_values;
 
+         // Build bubble chart
+
+        let trace1 = [
+        {
+            x: ids,
+            y: values,
+            text: labels,
+            mode: 'markers',
+            marker: {
+                color: ids,
+                size: values,
+            }
+        }
+        ];
+
+        let data1 = [trace1];
+
+        let bubble_layout = {
+            xaxis: { title: "OTU ID" },
+            hovermode: 'closest',
+            margin: {t:0}
+        };
+
+        Plotly.newPlot("bubble", data1, bubble_layout);
+        
         // Build bar chart
 
-        let trace = {
-            y: ids.slice(0, 10).map(otuID => 'OTU $(otuID}').reverse(),
+        let trace = [
+        {
+            y: ids.slice(0, 10).map(otuID => 'OTU ${otuID}').reverse(),
             x: values.slice(0, 10).reverse(),
             text: labels.slice(0, 10).reverse(),
             type: 'bar',
             orientation: 'h'
-        };
+        }
+        ];
 
         let data0 = [trace];
 
@@ -52,69 +79,9 @@ function buildChart(sample) {
 
         Plotly.newPlot('bar', data0, layout);
 
-        // Build bubble chart
-
-        let trace1 = {
-            x: ids,
-            y: values,
-            text: labels,
-            mode: 'markers',
-            marker: {
-                color: ids,
-                size: values,
-            }
-        };
-
-        let data1 = [trace1];
-
-        let bubble_layout = {
-            xaxis: { title: "OTU ID" },
-            hovermode: 'closest',
-        };
-
-        Plotly.newPlot("bubble", data1, bubble_layout);
+       
     });
 }
-
-
-
-
-//Create Gauge
-
-//let gauge_data = [
-    //{
-       // domain: { x: [0, 5], y: [0, 1] },
-      //  value: sample_data.wfreq,
-       // text: sample_data.wfreq,
-       // type: 'indicator',
-       // mode: 'gauge+number',
-       // delta: { reference: 10 },
-      //  gauge: {
-         //   axis: { range: [null, 9] },
-           // steps: [
-            //    { range: [0, 1], color: 'rgb(248, 243, 236)' },
-            //    { range: [1, 2], color: "rgb(239, 234, 220)" },
-            //    { range: [2, 3], color: "rgb(230, 225, 205)" },
-             //   { range: [3, 4], color: "rgb(218, 217, 190)" },
-             //   { range: [4, 5], color: "rgb(204, 209, 176)" },
-              //  { range: [5, 6], color: "rgb(189, 202, 164)" },
-             //   { range: [6, 7], color: "rgb(172, 195, 153)" },
-             //   { range: [7, 8], color: "rgb(153, 188, 144)" },
-             //   { range: [8, 9], color: "rgb(132, 181, 137)" },
-           // ],
-       // },
-   // },
-//];
-
-//let gauge_layout = {
-//    title: '<b>Belly Button Washing Frequency</b> <br>Scrubs Per Week',
-//    width: 300,
- //   height: 300
-//};
-
-//Plotly.newPlot("gauge", gauge_data, gauge_layout);
-    
-//}
 
 // Get data into inspector console
 function init() {
